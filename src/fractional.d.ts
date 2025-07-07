a3172498d94cebc82d48b3f31cecb7f38c280533
marcelo paschoal <marcelo.hlp@outlook.com>
2025-07-07 18:28:45 -0300

    refactor: removing fractional files

--- src/fractional.d.ts
+++
@@ -1,8 +0,0 @@
-declare module "fractional" {
-    export class Fraction {
-        constructor(numerator: number | string);
-        numerator: number;
-        denominator: number;
-        toString(): string;
-    }
-}
