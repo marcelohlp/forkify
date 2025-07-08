194fc42110b53272971909f6cd63789f6d2758e4
marcelo paschoal <marcelo.hlp@outlook.com>
2025-07-08 15:21:03 -0300

    refactor: adding style to the current recipe from  search result

--- src/js/controller.js
+++ src/js/controller.js
@@ -25,6 +25,8 @@ async function controlRecipes() {
 
         recipeView.renderSpinner();
 
+        resultsView.update(model.getSearchResultsPage());
+
         await model.loadRecipe(id);
 
         recipeView.render(model.state.recipe);
